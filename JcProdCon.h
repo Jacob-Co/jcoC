e1665adc58f8c555edda67d811b42edb9d7f703a
jco <jacobtco@gmail.com>
2025-11-07 11:00:58 +0800

    feat: added jc prod con

--- JcProdCon.h
+++ JcProdCon.h
@@ -14,6 +14,18 @@ create consumer of producer (producer*, function*)
 
 */
 
-struct JcProdCon {
-  long data_size;
+struct Jc_Prod_Con {
+  long prod_data_size;
+  void (*consumer_function)(void*);
+  /* add queue for data items*/
 };
+
+struct Jc_Prod_Con jc_prod_con_new(long prod_data_size, void(*consumer_function)(void*)) {
+  struct Jc_Prod_Con prod_con = {0};
+  prod_con.prod_data_size = prod_data_size;
+  prod_con.consumer_function = consumer_function;
+  /* initialize queue for data items*/
+  return prod_con;
+} 
+
+
