ebbed48026c0d26dc4bef5ee8ee385935225225e
jco <jacobtco@gmail.com>
2025-11-10 22:17:22 +0800

    feat: fixed some issues

--- JcProdCon.h
+++ JcProdCon.h
@@ -1,19 +1,3 @@
-/*
-producer write & forget
-
-user will create a producer
-
-produce(void *)
-ie read page id 4321
-consumer will be called by producer, then consumer will call consumer
-  with the void*
-
-Interface, create producer (callback*, function*)
-
-create consumer of producer (producer*, function*)
-
-*/
-
 #include <bits/pthreadtypes.h>
 #include <stdlib.h>
 #include <string.h>
@@ -25,32 +9,35 @@ struct Jc_Prod_Cons {
 
   int prod_count; 
 
-  void* data_item;
-  char is_data_item_valid;
+  void* data_item_ptr;
+  char is_data_item_fresh;
   pthread_mutex_t data_item_mtx;
+  pthread_cond_t data_item_mtx_condition;
 
   pthread_t threads[3];
 
-  pthread_cond_t cons_condition;
   pthread_mutex_t cons_condition_mtx;
+  pthread_cond_t cons_condition;
 };
 
 void* _consume(void*); 
 
 struct Jc_Prod_Cons* jc_prod_cons_new(long prod_data_size, void(*consumer_function)(void*)) {
   struct Jc_Prod_Cons* prod_cons = malloc(sizeof(struct Jc_Prod_Cons));
+  prod_cons->data_item_ptr = malloc(sizeof(prod_data_size));
   prod_cons->data_item_size = prod_data_size;
-  prod_cons->is_data_item_valid = 0;
+  prod_cons->is_data_item_fresh = 0;
   prod_cons->consumer_function = consumer_function;
   prod_cons->prod_count = 3;
 
   pthread_mutex_init(&prod_cons->data_item_mtx, NULL);
+  pthread_cond_init(&prod_cons->data_item_mtx_condition, NULL);
   pthread_mutex_init(&prod_cons->cons_condition_mtx, NULL);
   pthread_cond_init(&prod_cons->cons_condition, NULL);
 
-  pthread_create(&prod_cons->threads[0], NULL, _consume, (void*) &prod_cons);
-  pthread_create(&prod_cons->threads[1], NULL, _consume, (void*) &prod_cons);
-  pthread_create(&prod_cons->threads[2], NULL, _consume, (void*) &prod_cons);
+  pthread_create(&prod_cons->threads[0], NULL, _consume, (void*) prod_cons);
+  pthread_create(&prod_cons->threads[1], NULL, _consume, (void*) prod_cons);
+  pthread_create(&prod_cons->threads[2], NULL, _consume, (void*) prod_cons);
 
   return prod_cons;
 } 
@@ -66,14 +53,17 @@ void* _consume(void* ptr) {
 
     pthread_mutex_lock(&prod_cons->data_item_mtx);
 
-    if (!prod_cons->is_data_item_valid) {
+    if (!prod_cons->is_data_item_fresh) {
       pthread_mutex_unlock(&prod_cons->data_item_mtx);
       continue;
     }
 
     {
       void * consumer_data_item = malloc(prod_cons->data_item_size);
-      prod_cons->is_data_item_valid = 0;
+      memcpy(consumer_data_item, prod_cons->data_item_ptr, prod_cons->data_item_size);
+
+      prod_cons->is_data_item_fresh = 0;
+      pthread_cond_signal(&prod_cons->data_item_mtx_condition);
       pthread_mutex_unlock(&prod_cons->data_item_mtx);
 
       prod_cons->consumer_function(consumer_data_item);
@@ -84,23 +74,19 @@ void* _consume(void* ptr) {
   return NULL;
 }
 
-void produce(struct Jc_Prod_Cons* prod_cons, void* data_item) {
-  
-
+void jc_prod_cons_produce(struct Jc_Prod_Cons* prod_cons, void* data_item) {
   pthread_mutex_lock(&prod_cons->data_item_mtx);
-  memcpy(prod_cons->data_item, data_item, prod_cons->data_item_size);
-  prod_cons->is_data_item_valid = 1;
-  pthread_mutex_unlock(&prod_cons->data_item_mtx);
+  memcpy(prod_cons->data_item_ptr, data_item, prod_cons->data_item_size);
+  prod_cons->is_data_item_fresh = 1;
 
+  /*signal consumer*/
   pthread_mutex_lock(&prod_cons->cons_condition_mtx);
   pthread_cond_signal(&prod_cons->cons_condition);
   pthread_mutex_unlock(&prod_cons->cons_condition_mtx);
-  /*
-  todo: 
-    add unlock here for data item
-    signal consumer
-    block producer until consumer signals back
-  */
 
+  /*wait until data item is picked up by consumer*/
+  while(prod_cons->is_data_item_fresh) {
+    pthread_cond_wait(&prod_cons->data_item_mtx_condition, &prod_cons->data_item_mtx);
+  }
+  pthread_mutex_unlock(&prod_cons->data_item_mtx);
 }
-
---
+++ JcProdCon.test.c
@@ -0,0 +1,67 @@
+#include "JcProdCon.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+struct Arg {
+  int timeout;
+  int pos;
+  int value;
+  int* values;
+};
+
+void place_value(void*);
+
+int main(void) {
+  struct Jc_Prod_Cons* prod_cons = jc_prod_cons_new(sizeof(struct Arg), place_value);
+  int* my_values = malloc(sizeof(int) * 3);
+  struct Arg* a1 = malloc(sizeof(struct Arg));
+  struct Arg* a2 = malloc(sizeof(struct Arg));
+  struct Arg* a3 = malloc(sizeof(struct Arg));
+  struct Arg* a4 = malloc(sizeof(struct Arg));
+  a1->timeout = 1;
+  a1->pos = 0;
+  a1->value = 0;
+  a1->values = my_values;
+
+  a2->timeout = 10;
+  a2->pos = 1;
+  a2->value = 1;
+  a2->values = my_values;
+
+  a3->timeout = 10;
+  a3->pos = 2;
+  a3->value = 2;
+  a3->values = my_values;
+
+  a4->timeout = 1;
+  a4->pos = 0;
+  a4->value = 100;
+  a4->values = my_values;
+
+  jc_prod_cons_produce(prod_cons, (void *)a1);
+  jc_prod_cons_produce(prod_cons, (void *)a2);
+  jc_prod_cons_produce(prod_cons, (void *)a3);
+  jc_prod_cons_produce(prod_cons, (void *)a4);
+
+  sleep(60);
+
+  if (my_values[0] != 100) {
+    return 1;
+  }
+
+  if (my_values[1] != 1) {
+    return 1;
+  }
+
+  if (my_values[2] != 2) {
+    return 1;
+  }
+
+  return 0;
+}
+
+void place_value(void* arg) {
+  struct Arg* arg_arg = (struct Arg*) arg;
+  sleep(arg_arg->timeout);
+  arg_arg->values[arg_arg->pos] = arg_arg->value;
+}
