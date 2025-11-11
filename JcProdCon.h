4f6d3416e57b8f85de144f786645412ecfc76cc8
jco <jacobtco@gmail.com>
2025-11-11 19:58:00 +0800

    chore: added read me

--- JcProdCon.h
+++ JcProdCon.h
@@ -10,10 +10,11 @@ struct Jc_Prod_Cons {
   int prod_count; 
 
   void* data_item_ptr;
-  char is_data_item_filled;
+  char is_data_item_ready;
+
   pthread_mutex_t data_item_mtx;
-  pthread_cond_t data_item_mtx_condition_ready;
-  pthread_cond_t data_item_consumer_waiting;
+  pthread_cond_t data_item_ready_condition;
+  pthread_cond_t data_item_empty_condition;
 
   pthread_t threads[3];
 
@@ -27,12 +28,12 @@ struct Jc_Prod_Cons* jc_prod_cons_new(long prod_data_size, void(*consumer_functi
   struct Jc_Prod_Cons* prod_cons = malloc(sizeof(struct Jc_Prod_Cons));
   prod_cons->data_item_ptr = malloc(sizeof(prod_data_size));
   prod_cons->data_item_size = prod_data_size;
-  prod_cons->is_data_item_filled = 0;
+  prod_cons->is_data_item_ready = 0;
   prod_cons->consumer_function = consumer_function;
   prod_cons->prod_count = 3;
 
   pthread_mutex_init(&prod_cons->data_item_mtx, NULL);
-  pthread_cond_init(&prod_cons->data_item_mtx_condition_ready, NULL);
+  pthread_cond_init(&prod_cons->data_item_ready_condition, NULL);
   pthread_mutex_init(&prod_cons->cons_condition_mtx, NULL);
   pthread_cond_init(&prod_cons->cons_condition, NULL);
 
@@ -45,23 +46,24 @@ struct Jc_Prod_Cons* jc_prod_cons_new(long prod_data_size, void(*consumer_functi
 
 void* _consume(void* ptr) {
   struct Jc_Prod_Cons* prod_cons = (struct Jc_Prod_Cons*) ptr;
+  void* local_data_item_ptr;
+
   while(1) {
-    void* consumer_data_item;
     pthread_mutex_lock(&prod_cons->data_item_mtx);
 
-    while(!prod_cons->is_data_item_filled) {
-      pthread_cond_signal(&prod_cons->data_item_consumer_waiting);
-      pthread_cond_wait(&prod_cons->data_item_mtx_condition_ready, &prod_cons->data_item_mtx);
+    while(!prod_cons->is_data_item_ready) {
+      pthread_cond_wait(&prod_cons->data_item_ready_condition, &prod_cons->data_item_mtx);
     }
 
-    consumer_data_item = malloc(prod_cons->data_item_size);
-    memcpy(consumer_data_item, prod_cons->data_item_ptr, prod_cons->data_item_size);
-    prod_cons->is_data_item_filled = 0;
+    local_data_item_ptr = malloc(prod_cons->data_item_size);
+    memcpy(local_data_item_ptr, prod_cons->data_item_ptr, prod_cons->data_item_size);
+    prod_cons->is_data_item_ready = 0;
 
+    pthread_cond_signal(&prod_cons->data_item_empty_condition);
     pthread_mutex_unlock(&prod_cons->data_item_mtx);
 
-    prod_cons->consumer_function(consumer_data_item);
-    free(consumer_data_item);
+    prod_cons->consumer_function(local_data_item_ptr);
+    free(local_data_item_ptr);
   }
 
   return NULL;
@@ -69,14 +71,14 @@ void* _consume(void* ptr) {
 
 void jc_prod_cons_produce(struct Jc_Prod_Cons* prod_cons, void* data_item) {
   pthread_mutex_lock(&prod_cons->data_item_mtx);
-  while(prod_cons->is_data_item_filled) {
-    pthread_cond_wait(&prod_cons->data_item_consumer_waiting, &prod_cons->data_item_mtx);
+
+  while(prod_cons->is_data_item_ready) {
+    pthread_cond_wait(&prod_cons->data_item_empty_condition, &prod_cons->data_item_mtx);
   }
 
   memcpy(prod_cons->data_item_ptr, data_item, prod_cons->data_item_size);
-  prod_cons->is_data_item_filled = 1;
+  prod_cons->is_data_item_ready = 1;
 
-  pthread_cond_signal(&prod_cons->data_item_mtx_condition_ready);
-
+  pthread_cond_signal(&prod_cons->data_item_ready_condition);
   pthread_mutex_unlock(&prod_cons->data_item_mtx);
 }
