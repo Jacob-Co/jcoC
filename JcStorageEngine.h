0e2809232f40370ee0374ea39299c4a10ac8827a
jco <jacobtco@gmail.com>
2025-11-15 00:14:14 +0800

    feat: added tuples and offsets

--- JcStorageEngine.h
+++ JcStorageEngine.h
@@ -1,7 +1,13 @@
 struct Jc_Storage_Engine {
   int fd;
+  long current_storage_offset;
 };
 
-struct Jc_Storage_Engine jc_storage_engine_new(char* name) {
-  
+struct Jc_Tuple_Id {
+  long storage_offset;
+  int offset;
 };
+
+struct Jc_Storage_Engine* jc_storage_engine_new(char* name) {
+};
+
