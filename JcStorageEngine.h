0e5844d735292587afea47ad4ba6f0508839b4af
jco <jacobtco@gmail.com>
2025-11-16 02:37:49 +0800

    feat: filled in fd

--- JcStorageEngine.h
+++ JcStorageEngine.h
@@ -1,6 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#ifndef BLOCK_SIZE
+#define BLOCK_SIZE 4096
+#endif
 struct Jc_Storage_Engine {
   int fd;
-  long current_storage_offset;
+  long current_tuple_offset;
 };
 
 struct Jc_Tuple_Id {
@@ -8,6 +16,36 @@ struct Jc_Tuple_Id {
   int offset;
 };
 
+
+/*
+for v 0
+db table layout:
+int version number
+long current tuple offset
+tuples*
+
+*/
+
 struct Jc_Storage_Engine* jc_storage_engine_new(char* name) {
+  struct Jc_Storage_Engine * storage_engine = malloc(sizeof(struct Jc_Storage_Engine));
+  struct stat my_stat;
+
+  storage_engine->fd = open(name, O_CREAT | O_RDWR | O_FSYNC );
+  fstat(storage_engine->fd, &my_stat);
+
+  if (my_stat.st_size == 0) {
+    /*initialize db layout with version and first tuple offset*/ 
+
+    return storage_engine;
+  } else {
+    /* read and set current tuple offset */
+
+    return storage_engine
+  }
+};
+
+void jc_storage_engine_store(void* data, int size) {
+  // fwrite(data, size, size, FILE *)
+  
 };
 
