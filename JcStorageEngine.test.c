0a610c9bf5b2341899056c76810a56a1f8ab61f6
jco <jacobtco@gmail.com>
2025-11-19 21:21:26 +0800

    feat: added test for re-writes

--- JcStorageEngine.test.c
+++ JcStorageEngine.test.c
@@ -6,8 +6,73 @@
 #include "JcStorageEngine.h"
 
 int compare_bytes(char*, long, char*, long);
+int append_test(void);
+int re_write_test(void);
 
 int main(void) {
+  if (append_test()) {
+    return 1;
+  }
+
+  if (re_write_test()) {
+    return 1;
+  }
+
+  /*
+  delete
+  append write
+  delete
+  make sure the get offset is adjusted
+  */
+  return 0;
+}
+
+int re_write_test(void) {
+  int test_fd;
+  long block_size = 4096;
+  struct Jc_Storage_Engine* storage_engine = jc_storage_engine_new("test", block_size);
+
+  remove("test");
+  test_fd = open("test", O_TMPFILE);
+  {
+    /*
+      testing re-write
+      append write
+      append write
+      write on first
+      make sure first equals the rewrite
+    */
+    void* data_1 = calloc(block_size, 1);
+    int test_val_1 = 100;
+
+    void* data_2 = calloc(block_size, 1);
+    int test_val_2 = 200;
+
+    void* result_val = malloc(block_size);
+
+    memcpy(data_1, &test_val_1, sizeof(test_val_1));
+    memcpy(data_2, &test_val_2, sizeof(test_val_2));
+
+    jc_storage_engine_write(storage_engine, data_1, 0);
+    jc_storage_engine_write(storage_engine, data_2, 0);
+
+    jc_storage_engine_get(storage_engine, result_val, 0);
+
+    /* should not be equal with data_1 as this was overwritten */
+    if (compare_bytes(data_1, block_size, result_val, block_size)) {
+      return 1;
+    }
+
+    if (!compare_bytes(data_2, block_size, result_val, block_size)) {
+      return 1;
+    }
+  }
+
+  close(test_fd);
+  return 0;
+}
+
+int append_test(void) {
   int test_fd;
   long block_size = 4096;
   struct Jc_Storage_Engine* storage_engine = jc_storage_engine_new("test", block_size);
@@ -15,7 +80,6 @@ int main(void) {
   remove("test");
   test_fd = open("test", O_TMPFILE);
 
-  /*append test*/
   {
     void* data_append_1 = calloc(block_size, 1);
     int test_val_1 = 100;
@@ -44,25 +108,6 @@ int main(void) {
       return 1;
     }
   }
-
-  /*
-  test append writes
-  append write
-  append write
-  then get both and make sure values match
-
-
-  testing re-write
-  append write
-  append write
-  write on first
-  make sure first equals the rewrite
-
-  delete
-  append write
-  delete
-  make sure the get offset is adjusted
-  */
 
   close(test_fd);
   return 0;
